74dc66bd77f9ee7cebdb3a701271b8a80e05ab45
Ben Waters <bsawyerwaters@gmail.com>
2016-02-29 16:02:28 +0200

    Let's get talking

--- restfulROSserver.py
+++ restfulROSserver.py
@@ -1,10 +1,11 @@
 from flask import request
-from flask.ext.api import FlaskAPI
+from flask.ext.api import FlaskAPI, status
 
 from rosapi import proxy, objectutils
 
 from flask.ext.api.exceptions import APIException
 
+from fred import get_fred_command
 
 class ROSUnavailable(APIException):
     status_code = 500
@@ -33,10 +34,30 @@ def list_topics():
     :return:
     """
     topics = proxy.get_topics()
+    type_filter = request.args.get('type', '')
+    topic_filter = request.args.get('topic', '')
+    if type_filter != '':
+        return {'topics': [{'topic': topic,
+                            'data': objectutils.get_typedef_recursive(proxy.get_topic_type(topic))} for topic in topics
+                           if type_filter in proxy.get_topic_type(topic)]}
+    elif type_filter != '':
+        return {'topics': [{'topic': topic,
+                            'data': objectutils.get_typedef_recursive(proxy.get_topic_type(topic))} for topic in topics
+                           if topic_filter in topic]}
     return {'topics': [{'topic': topic,
-                        'data': dict((k, v) for k, v in objectutils.get_typedef_recursive(proxy.get_topic_type(topic)).items() if
-                                     k != "examples" or k != "fieldarraylen")} for topic in topics]}
+                        'data': objectutils.get_typedef_recursive(proxy.get_topic_type(topic))} for topic in topics]}
 
+@app.route('/publish', methods=['POST'])
+def publish():
+    op = request.data.get('op', None)
+    ros_type = request.data.get('type', None)
+    topic = request.data.get('topic', None)
+    msg = request.data.get('msg', None)
+    if op is None or ros_type is None or topic is None or msg is None:
+        return status.HTTP_400_BAD_REQUEST
+    else:
+        print get_fred_command(msg)
+    return 'Ok', status.HTTP_200_OK
 
 @app.route('/type/{<string:name>}/', methods=['GET'])
 def get_type(name):
